0f6c74510175b7f468c07a759b1f2910b2738f2c
Belén Molina del Campo <bmolinadelcampo@gmail.com>
2016-05-19 10:10:54 +0100

    Initial Commit

---
+++ ViagogoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViagogoTest
+//
+//  Created by Belén Molina del Campo on 19/05/2016.
+//  Copyright © 2016 Belén Molina del Campo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViagogoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViagogoTest
+//
+//  Created by Belén Molina del Campo on 19/05/2016.
+//  Copyright © 2016 Belén Molina del Campo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
